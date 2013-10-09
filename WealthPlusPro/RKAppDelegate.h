24151406d7f44c589c0fab9dfd025a63d68bf5ef
Evan Wu <wuzhiyong890926@gmail.com>
2013-10-09 17:36:45 +0800

    first commit

---
+++ WealthPlusPro/RKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RKAppDelegate.h
+//  WealthPlusPro
+//
+//  Created by Evan on 13-10-9.
+//  Copyright (c) 2013年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WealthPlusPro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WealthPlusPro
+//
+//  Created by Evan on 13-10-9.
+//  Copyright (c) 2013年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKAppDelegate class]));
+    }
+}
